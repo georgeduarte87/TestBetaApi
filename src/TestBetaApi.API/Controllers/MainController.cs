898b6647aa0b289fd25b3f8b213e86a2b862a6a5
George Duarte <contato@georgeduarte.com.br>
2022-05-22 13:16:55 -0300

    Implementação da FornecedorController

--- src/TestBetaApi.API/Controllers/MainController.cs
+++ src/TestBetaApi.API/Controllers/MainController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq;
+using TestBetaApi.Business.Intefaces;
+using TestBetaApi.Business.Notificacoes;
 
 namespace TestBetaApi.API.Controllers
 {
@@ -6,10 +10,59 @@ namespace TestBetaApi.API.Controllers
     [ApiController]
     public abstract class MainController : ControllerBase
     {
-        // validação de notificação de erros
+        private readonly INotificador _notificador;
 
-        // validação de modelstate
+        public MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
 
-        //validação de operação de negócios
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if(OperacaoValida())
+            {
+                return Ok( new
+                { 
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                erros = _notificador.ObterNotificacao().Select(n => n.Mensagem)
+            });
+        }
+
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotificarErroModelInvalida(modelState);
+            return CustomResponse();
+        }
+
+        protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach(var erro in erros)
+            {
+                var errorMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(errorMsg);
+            }
+
+
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
     }
 }
