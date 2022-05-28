688ccd1df7a64020909f12e22fadf6ac8670b185
George Duarte <contato@georgeduarte.com.br>
2022-05-28 14:46:30 -0300

    Retornando dados do usuário no response Token

--- src/TestBetaApi.API/Controllers/AuthController.cs
+++ src/TestBetaApi.API/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -82,7 +83,7 @@ namespace TestBetaApi.API.Controllers
             return CustomResponse(loginUser);
         }
 
-        private async Task<string> GerarJwt(string email)
+        private async Task<LoginResponseViewModel> GerarJwt(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -115,7 +116,19 @@ namespace TestBetaApi.API.Controllers
 
             var encodeToken = tokenHandler.WriteToken(token);
 
-            return encodeToken;
+            var response = new LoginResponseViewModel
+            {
+                AccessToken = encodeToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
         }
 
         private static long ToUnixEpochDate(DateTime date)
